91c27480df65d0a48e624f6a92f1364e53af7aee
igm503 <igm503@gmail.com>
2024-03-04 00:57:42 -0600

    fixed window movement bug from faulty copilot keymapping

--- mappings.lua
+++ mappings.lua
@@ -31,15 +31,6 @@ M.copilot = {
        {replace_keycodes = true, nowait=true, silent=true, expr=true, noremap=true}
     }
   },
-  n = {
-    ["<C-l>"] = {
-      function()
-        vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
-      end,
-      "Copilot Accept",
-       {replace_keycodes = true, nowait=true, silent=true, expr=true, noremap=true}
-    }
-  }
 }
 
 return M
