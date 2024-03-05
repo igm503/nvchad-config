2f7ce5a0e944faa9c0e0cf4e8f82ec37cb3dbf1d
igm503 <igm503@gmail.com>
2024-03-05 11:27:19 -0600

    changed copilot accept to ctrl ; to regain ctrl l functions in i mode

--- mappings.lua
+++ mappings.lua
@@ -23,12 +23,12 @@ local M = {}
 
 M.copilot = {
   i = {
-    ["<C-l>"] = {
+    ["<C-;>"] = {
       function()
         vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
       end,
       "Copilot Accept",
-       {replace_keycodes = true, nowait=true, silent=true, expr=true, noremap=true}
+      {replace_keycodes = true, nowait=true, silent=true, expr=true, noremap=true}
     }
   },
 }
