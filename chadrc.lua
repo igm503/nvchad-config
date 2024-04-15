fac52e8f76877f685995ef5d4dd38df151f27fd8
igm503 <igm503@gmail.com>
2024-04-15 14:03:59 -0500

    changed number colors

--- chadrc.lua
+++ chadrc.lua
@@ -27,6 +27,8 @@ M.ui = {
         ["@function.builtin"] = { fg = "#DCDCAA" },
         ["@function.method.call"] = { fg = "#DCDCAA" },
         ["@attribute"] = { fg = "#DCDCAA" },
+        ["Number"] = { fg = "#B5CEA8" },
+        ["@number.float"] = { fg = "#B5CEA8" },
         ["@string"] = { fg = "#cc937a" },
         ["Comment"] = { fg = "#738e9e" },
         ["@punctuation.delimiter"] = { fg = "#dbdad9" },
