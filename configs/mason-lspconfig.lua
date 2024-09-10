67958aff447294613307eb533e76a9db3413c3da
igm503 <igm503@gmail.com>
2024-09-10 16:50:39 -0500

    added omnisharp; added mason-lspconfig

---
+++ configs/mason-lspconfig.lua
@@ -0,0 +1,26 @@
+require("mason-lspconfig").setup()
+
+local configs = require("plugins.configs.lspconfig")
+local on_attach = configs.on_attach
+local capabilities = configs.capabilities
+
+require("mason-lspconfig").setup_handlers {
+  -- The first entry (without a key) will be the default handler
+  -- and will be called for each installed server that doesn't have
+  -- a dedicated handler.
+  function(server_name) -- default handler (optional)
+    require("lspconfig")[server_name].setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+    }
+  end,
+  -- Next, you can provide a dedicated handler for specific servers.
+  -- For example, a handler override for the `rust_analyzer`:
+  ["html"] = function()
+    require("lspconfig")["html"].setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+      filetypes = { "html", "htm", "htmldjango" },
+    }
+  end
+}
