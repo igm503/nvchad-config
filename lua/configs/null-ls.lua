5e5d87477c831e48f78a3356e400234c32a7eb86
igm503 <igm503@gmail.com>
2025-01-13 14:14:49 -0600

    prettierd should have been prettier in null-ls config

--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -8,7 +8,7 @@ local opts = {
       extra_args = { "--line-length", "100" },
     },
     null_ls.builtins.formatting.csharpier,
-    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.prettier,
   },
   -- on_attach = function(client, bufnr)
   --  if client.supports_method("textDocument/formatting") then
