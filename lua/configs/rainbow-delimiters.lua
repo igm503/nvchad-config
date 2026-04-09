bdcd4cd8fd09dd0e44205fa247519e7aadf480cb
Ian <igm503@gmail.com>
2026-04-09 11:30:11 -0400

    better rainbow delimiters fix

--- lua/configs/rainbow-delimiters.lua
+++ lua/configs/rainbow-delimiters.lua
@@ -21,3 +21,16 @@ require("rainbow-delimiters.setup").setup {
   },
   blacklist = { "NvimTree", "noice", "notify", "terminal", "NvTerm_float" },
 }
+local lib = require "rainbow-delimiters.lib"
+local original_attach = lib.attach
+lib.attach = function(bufnr)
+  local lang = vim.treesitter.language.get_lang(vim.bo[bufnr].filetype)
+  if not lang then
+    return
+  end
+  local ok = pcall(vim.treesitter.language.inspect, lang)
+  if not ok then
+    return
+  end
+  original_attach(bufnr)
+end
